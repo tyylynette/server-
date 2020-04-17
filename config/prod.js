288a03cb02c8b6ddeaecaf1aaaa1c8722681a0da
tyylynette <tyylynette@gmail.com>
2020-04-17 11:12:50 +0800

    removed keys

--- config/prod.js
+++ config/prod.js
@@ -5,12 +5,3 @@ module.exports = {
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY
 };
-
-// live server mongoDB
-//mongodb+srv://tyylynette:eZOXxxCYcfq74ysU@cluster0-cmnpr.mongodb.net/test?retryWrites=true&w=majority
-
-//prod googleClientID
-//1078739925738-1hh7hmopbqts7kugu80lrqfeln8md6ep.apps.googleusercontent.com
-
-//prod client secret
-//3V5BJTG5GprbN6tMbPvEM8y6
