bc4072fb9cf6a59d3d3bc398f0965567f1263467
tyylynette <tyylynette@gmail.com>
2020-04-21 16:00:32 +0800

    remove build from gitignore

---
+++ client/build/precache-manifest.3bfa121c7bb6fedd2d79145863025e8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "186d1af89475bb5cdf467534b181525e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b217da499fa97b95db53",
+    "url": "/static/css/2.658eb2da.chunk.css"
+  },
+  {
+    "revision": "b217da499fa97b95db53",
+    "url": "/static/js/2.62714d54.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.62714d54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb03bb8d3a22e711e379",
+    "url": "/static/js/main.fc3a258f.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  }
+]);
