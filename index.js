48c5ee01d197f60f38d8692365551ec7b84b2855
tyylynette <tyylynette@gmail.com>
2020-04-29 15:15:13 +0800

    changed build path

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ require('./routes/surveyRoutes')(app);
 if (process.env.NODE_ENV === 'production') {
   //first make sure expresss will serve up production assets
   //like our main.js file, or main.css file
-  app.use(express.static('client/build')); //look into client directory for matching file
+  app.use(express.static(path.join(__dirname, 'client/build'))); //look into client directory for matching file
 
   //otherwise express will serve up our index.html file
   //if it can't find the file in client/build dir
